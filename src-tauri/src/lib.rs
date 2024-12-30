1d2cd36492cffef67fbce8891862ecdc7f42051a
SNKK62 <koki.seno62@gmail.com>
2024-12-30 22:37:55 +0900

    add system-tray and prevent quit when closing last window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::tray::TrayIconBuilder;
+use tauri::{Manager, WindowEvent};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +9,20 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app)?;
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                // ウィンドウのクローズイベントをカスタマイズ
+                api.prevent_close();
+                // ウィンドウを非表示にする
+                window.hide().unwrap();
+            }
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
